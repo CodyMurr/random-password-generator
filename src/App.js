e66ef95024700455ff2e7d0f19b5809797fe50fe
CodyMurr <codakmurraki@gmail.com>
2022-05-16 20:23:56 -0500

    add clipboard copy function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import './App.css';
 import { generate } from './utilities/generator';
+import { copyPW } from './utilities/helper';
 import { useState } from 'react';
 import { FaCopy, FaUndo } from 'react-icons/fa';
 import PasswordForm from './components/PasswordForm/PasswordForm';
 
 export default function App() {
 	const [result, setResult] = useState(null);
+	const [clipboard, setClipboard] = useState({
+		text: '',
+		result: '',
+	});
 
 	function getPassword(arr, len) {
 		const pw = generate(arr, len);
 		setResult(pw);
 	}
+
+	function copy() {
+		if (result) {
+			copyPW(result, setClipboard);
+		} else {
+			return;
+		}
+
+		console.log(clipboard);
+	}
 	return (
 		<div className='App flex col'>
 			<section className='output flex col'>
 				<h1>{result ? result : 'Generate Password Below'}</h1>
-				<section className='flex icons'>
+				<section className='flex col icons'>
 					{/* implement clipboard functionality */}
-					<FaCopy size={50} color='#C33C54' />
-					&nbsp;
-					<FaUndo size={50} color='#C33C54' />
-					&nbsp;
+					<FaCopy
+						className='copy-ico'
+						size={50}
+						color='#C33C54'
+						onClick={copy}
+					/>
+					<p className={clipboard.result === 'Copied!' && 'copied'}>
+						{clipboard.result}
+					</p>
+					{/* &nbsp; */}
+					{/* <FaUndo size={50} color='#C33C54' />
+					&nbsp; */}
 				</section>
 			</section>
 			<section className='form-container flex'>
--- src/utilities/helper.js
+++ src/utilities/helper.js
@@ -1,3 +1,17 @@
 export function capitalize(str) {
-  return str.slice(0, 1)[0].toUpperCase().concat(str.slice(1));
+	return str.slice(0, 1)[0].toUpperCase().concat(str.slice(1));
+}
+
+export function copyPW(val, cb) {
+	try {
+		navigator.clipboard.writeText(val);
+		cb({
+			text: val,
+			result: 'Copied!',
+		});
+	} catch (err) {
+		cb({
+			result: 'Copy Failed',
+		});
+	}
 }
