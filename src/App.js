0c55b18c0526014bba2a54127aea19bf7cee3349
CodyMurr <codakmurraki@gmail.com>
2022-06-02 18:19:48 -0500

    update copy icon to clipboard

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
 import './App.css';
 import { useContext } from 'react';
-import { FaCopy /* FaUndo */ } from 'react-icons/fa';
+import { FaClipboard, FaClipboardCheck } from 'react-icons/fa';
 import PasswordForm from './components/PasswordForm/PasswordForm';
 import PasswordContext from './context/PasswordContext';
 
 export default function App() {
-	const { result, getPassword, clipboard, copied, copy } =
-		useContext(PasswordContext);
+  const { result, getPassword, clipboard, copied, copy } =
+    useContext(PasswordContext);
 
-	return (
-		<div className='App flex col'>
-			<section className='output flex'>
-				<h1>{result ? result : 'Generate Password Below'}</h1>
-				<section className='flex col icons'>
-					<FaCopy
-						className='copy-ico'
-						size={50}
-						color='#C33C54'
-						onClick={copy}
-					/>
-					{copied === true ? (
-						<p className='copied'>{clipboard.result}</p>
-					) : (
-						<p> </p>
-					)}
-				</section>
-			</section>
-			<section className='form-container flex'>
-				<PasswordForm getPassword={getPassword} />
-			</section>
-		</div>
-	);
+  return (
+    <div className='App flex col'>
+      <section className='output flex'>
+        <h1>{result ? result : 'Generate Password Below'}</h1>
+        <section className='flex col icons'>
+          {copied ? (
+            <>
+              <FaClipboardCheck
+                className='copy-ico'
+                size={50}
+                color='#C33C54'
+              />
+              <p className='copied'>{clipboard.result}</p>
+            </>
+          ) : (
+            <>
+              <FaClipboard
+                className='copy-ico'
+                size={50}
+                color='#C33C54'
+                onClick={copy}
+              />
+              <p> </p>
+            </>
+          )}
+        </section>
+      </section>
+      <section className='form-container flex'>
+        <PasswordForm getPassword={getPassword} />
+      </section>
+    </div>
+  );
 }
--- src/context/PasswordContext.jsx
+++ src/context/PasswordContext.jsx
@@ -5,109 +5,111 @@ import { copyPW } from '../utilities/helper';
 const PasswordContext = createContext();
 
 export function PasswordProvider({ children }) {
-	const [lengthValues, setLengthValues] = useState({
-		min: 5,
-		max: 30,
-	});
-	const [charCt, setCharCt] = useState(5);
-	const [passParams, setPassParams] = useState([]);
-	const [result, setResult] = useState(null);
-	const [formData, setFormData] = useState({});
-	const [isChecked, setIsChecked] = useState({
-		lowercase: false,
-		uppercase: false,
-		numbers: false,
-		symbols: false,
-	});
-	const [clipboard, setClipboard] = useState({
-		text: '',
-		result: '',
-	});
-	const [copied, setCopied] = useState(false);
+  const [lengthValues, setLengthValues] = useState({
+    min: 5,
+    max: 30,
+  });
+  const [charCt, setCharCt] = useState(5);
+  const [passParams, setPassParams] = useState([]);
+  const [result, setResult] = useState(null);
+  const [formData, setFormData] = useState({});
+  const [isChecked, setIsChecked] = useState({
+    lowercase: false,
+    uppercase: false,
+    numbers: false,
+    symbols: false,
+  });
+  const [clipboard, setClipboard] = useState({
+    text: '',
+    result: '',
+  });
+  const [copied, setCopied] = useState(false);
 
-	function clearForm(obj, cb) {
-		const keys = Object.keys(obj);
-		setCharCt(lengthValues.min);
-		setPassParams([]);
-		let idx = 0;
-		while (idx < keys.length) {
-			cb({
-				[obj[keys[idx]]]: false,
-			});
-			idx++;
-		}
-		setResult(null);
-		setCopied(false);
-	}
+  function clearForm(obj, cb) {
+    const keys = Object.keys(obj);
+    setCharCt(lengthValues.min);
+    setPassParams([]);
+    let idx = 0;
+    while (idx < keys.length) {
+      cb({
+        [obj[keys[idx]]]: false,
+      });
+      idx++;
+    }
+    setResult(null);
+    setCopied(false);
+  }
 
-	function addParams(param, cb) {
-		setPassParams([...passParams, param]);
-		cb();
-	}
+  function addParams(param, cb) {
+    setPassParams([...passParams, param]);
+    cb();
+  }
 
-	function deleteParams(param, cb) {
-		setPassParams(passParams.filter((el) => el !== param));
-		cb();
-	}
+  function deleteParams(param, cb) {
+    setPassParams(passParams.filter((el) => el !== param));
+    cb();
+  }
 
-	function adjustLength(num) {
-		setCharCt(num);
-	}
+  function adjustLength(num) {
+    setCharCt(num);
+  }
 
-	function getPassword(arr, len) {
-		const pw = generate(arr, len);
-		setResult(pw);
-	}
+  function getPassword(arr, len) {
+    const pw = generate(arr, len);
+    setResult(pw);
+  }
 
-	function handleChange(e) {
-		e.preventDefault();
-		setFormData({
-			...formData,
-			[e.target.name]: e.target.value,
-		});
-	}
+  function handleChange(e) {
+    e.preventDefault();
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
 
-	function submitForm(cb) {
-		setLengthValues(formData);
-		setTimeout(() => {
-			cb();
-		}, 500);
-	}
+  function submitForm(cb) {
+    setLengthValues(formData);
+    setTimeout(() => {
+      cb();
+    }, 500);
+  }
 
-	function copy() {
-		if (result) {
-			copyPW(result, setClipboard);
-		} else {
-			return;
-		}
-		setCopied(true);
-	}
+  function copy() {
+    if (result) {
+      copyPW(result, setClipboard);
+    } else {
+      return;
+    }
+    setCopied(true);
+    console.log(result);
+  }
 
-	return (
-		<PasswordContext.Provider
-			value={{
-				lengthValues,
-				charCt,
-				passParams,
-				result,
-				formData,
-				isChecked,
-				clipboard,
-				copied,
-				setCopied,
-				setIsChecked,
-				clearForm,
-				addParams,
-				deleteParams,
-				adjustLength,
-				getPassword,
-				handleChange,
-				submitForm,
-				copy,
-			}}>
-			{children}
-		</PasswordContext.Provider>
-	);
+  return (
+    <PasswordContext.Provider
+      value={{
+        lengthValues,
+        charCt,
+        passParams,
+        result,
+        formData,
+        isChecked,
+        clipboard,
+        copied,
+        setCopied,
+        setIsChecked,
+        clearForm,
+        addParams,
+        deleteParams,
+        adjustLength,
+        getPassword,
+        handleChange,
+        submitForm,
+        copy,
+      }}
+    >
+      {children}
+    </PasswordContext.Provider>
+  );
 }
 
 export default PasswordContext;
