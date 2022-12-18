c60a1b32b18ef1f40fc5c092c3cb5493a946fa98
CodyMurr <codakmurraki@gmail.com>
2022-12-17 22:09:29 -0600

    update rendered message

--- src/context/PasswordContext.jsx
+++ src/context/PasswordContext.jsx
@@ -5,122 +5,126 @@ import { copyPW, truncate } from '../utilities/helper';
 const PasswordContext = createContext();
 
 export function PasswordProvider({ children }) {
-  const [lengthValues, setLengthValues] = useState({
-    min: 5,
-    max: 30,
-  });
-  const [charCt, setCharCt] = useState(5);
-  const [passParams, setPassParams] = useState([]);
-  const [formData, setFormData] = useState({});
-  const [isChecked, setIsChecked] = useState({
-    lowercase: false,
-    uppercase: false,
-    numbers: false,
-    symbols: false,
-  });
-  const [clipboard, setClipboard] = useState({
-    text: '',
-    result: '',
-  });
-  const [showPassword, setShowPassword] = useState(true);
-  const [copied, setCopied] = useState(false);
-  const [result, setResult] = useState(null);
-  const [shortened, setShortened] = useState(null);
+	const [lengthValues, setLengthValues] = useState({
+		min: 5,
+		max: 30,
+	});
+	const [charCt, setCharCt] = useState(5);
+	const [passParams, setPassParams] = useState([]);
+	const [formData, setFormData] = useState({});
+	const [isChecked, setIsChecked] = useState({
+		lowercase: false,
+		uppercase: false,
+		numbers: false,
+		symbols: false,
+	});
+	const [clipboard, setClipboard] = useState({
+		text: '',
+		result: '',
+	});
+	const [showPassword, setShowPassword] = useState(true);
+	const [copied, setCopied] = useState(false);
+	const [result, setResult] = useState(null);
+	const [shortened, setShortened] = useState(null);
 
-  function clearForm(obj, cb) {
-    const keys = Object.keys(obj);
-    setCharCt(lengthValues.min);
-    setPassParams([]);
-    let idx = 0;
-    while (idx < keys.length) {
-      cb({
-        [obj[keys[idx]]]: false,
-      });
-      idx++;
-    }
-    setResult(null);
-    setCopied(false);
-  }
+	function clearForm(obj, cb) {
+		const keys = Object.keys(obj);
+		setCharCt(lengthValues.min);
+		setPassParams([]);
+		let idx = 0;
+		while (idx < keys.length) {
+			cb({
+				[obj[keys[idx]]]: false,
+			});
+			idx++;
+		}
+		setResult(null);
+		setCopied(false);
+	}
 
-  function addParams(param, cb) {
-    setPassParams([...passParams, param]);
-    cb();
-  }
+	function addParams(param, cb) {
+		setPassParams([...passParams, param]);
+		cb();
+	}
 
-  function deleteParams(param, cb) {
-    setPassParams(passParams.filter((el) => el !== param));
-    cb();
-  }
+	function deleteParams(param, cb) {
+		setPassParams(passParams.filter((el) => el !== param));
+		cb();
+	}
 
-  function adjustLength(num) {
-    setCharCt(num);
-  }
+	function adjustLength(num) {
+		setCharCt(num);
+	}
 
-  function getPassword(arr, len) {
-    const pw = generate(arr, len);
-    if (len > 15) {
-      setShowPassword(false);
-      truncate(pw, len - 5, setShortened);
-    }
-    setResult(pw);
-  }
+	function getPassword(arr, len) {
+		const pw = generate(arr, len);
+		if (len > 15) {
+			setShowPassword(false);
+			truncate(pw, setShortened);
+		}
+		setResult(pw);
+		setClipboard({
+			text: '',
+			result: '',
+		});
+		setCopied(false);
+	}
 
-  function toggleShowPw() {
-    setShowPassword(!showPassword);
-  }
-  function handleChange(e) {
-    e.preventDefault();
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  }
+	function toggleShowPw() {
+		setShowPassword(!showPassword);
+	}
+	function handleChange(e) {
+		e.preventDefault();
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value,
+		});
+	}
 
-  function submitForm(cb) {
-    setLengthValues(formData);
-    setTimeout(() => {
-      cb();
-    }, 500);
-  }
+	function submitForm(cb) {
+		setLengthValues(formData);
+		setTimeout(() => {
+			cb();
+		}, 500);
+	}
 
-  function copy() {
-    if (result) {
-      copyPW(result, setClipboard);
-    } else {
-      return;
-    }
-    setCopied(true);
-  }
+	function copy() {
+		if (result) {
+			copyPW(result, setClipboard);
+		} else {
+			return;
+		}
+		setCopied(true);
+	}
 
-  return (
-    <PasswordContext.Provider
-      value={{
-        lengthValues,
-        charCt,
-        passParams,
-        result,
-        formData,
-        isChecked,
-        clipboard,
-        copied,
-        showPassword,
-        shortened,
-        setCopied,
-        setIsChecked,
-        clearForm,
-        addParams,
-        deleteParams,
-        toggleShowPw,
-        adjustLength,
-        getPassword,
-        handleChange,
-        submitForm,
-        copy,
-      }}
-    >
-      {children}
-    </PasswordContext.Provider>
-  );
+	return (
+		<PasswordContext.Provider
+			value={{
+				lengthValues,
+				charCt,
+				passParams,
+				result,
+				formData,
+				isChecked,
+				clipboard,
+				copied,
+				showPassword,
+				shortened,
+				setCopied,
+				setIsChecked,
+				clearForm,
+				addParams,
+				deleteParams,
+				toggleShowPw,
+				adjustLength,
+				getPassword,
+				handleChange,
+				submitForm,
+				copy,
+			}}>
+			{children}
+		</PasswordContext.Provider>
+	);
 }
 
 export default PasswordContext;
--- src/utilities/helper.js
+++ src/utilities/helper.js
@@ -1,22 +1,24 @@
 export function capitalize(str) {
-  return str.slice(0, 1)[0].toUpperCase().concat(str.slice(1));
+	return str.slice(0, 1)[0].toUpperCase().concat(str.slice(1));
 }
 
 export function copyPW(val, cb) {
-  try {
-    navigator.clipboard.writeText(val);
-    cb({
-      text: val,
-      result: 'Copied!',
-    });
-  } catch (err) {
-    cb({
-      result: 'Copy Failed',
-    });
-  }
+	try {
+		navigator.clipboard.writeText(val);
+		cb({
+			text: val,
+			result: 'Copied!',
+		});
+	} catch (err) {
+		cb({
+			result: 'Copy Failed',
+		});
+	}
 }
 
-export function truncate(str, max, cb) {
-  let copy = str.slice(0, max - 3);
-  cb(`${copy}...`);
+export function truncate(str, cb) {
+	const length = str.length;
+	const hidden = '*'.repeat(length);
+
+	cb(hidden);
 }
